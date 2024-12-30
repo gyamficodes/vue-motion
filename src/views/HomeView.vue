7fe0f280c64957139fcc8dd093ed865027b3dd49
gyamficodes <gyamfijohn08@gmail.com>
2024-12-30 21:35:45 +0000

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-2xl font-bold underline">Hello, Tailwind CSS!</h1>
+  <p>This is a Vue 3 component using Tailwind CSS.</p>
+</template>
+
+<style></style>

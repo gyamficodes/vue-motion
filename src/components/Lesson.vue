e9e69c1750a346fa4d7ae8aa9df5673c26db00b3
gyamficodes <gyamfijohn08@gmail.com>
2025-01-01 01:40:07 +0000

    update

---
+++ src/components/Lesson.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>{{ state.count }}</h1>
+      <button @click="hanfleIncrement">Add</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const state = reactive({count:0});
+
+const hanfleIncrement = () => {
+    state.count++;
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from "../components/HelloWorld.vue"
+import Lesson from "@/components/Lesson.vue";
 </script>
 
 <template>
    <HelloWorld/>
+   <Lesson/>
 </template>
 
 <style></style>

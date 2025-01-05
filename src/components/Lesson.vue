29cc71dfaa7c6ca0772223e2cae4f5dc9587f46f
gyamficodes <gyamfijohn08@gmail.com>
2025-01-05 21:36:33 +0000

    update

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,22 +1,46 @@
 <template>
-    <div>
-      <h1>{{ state.count }}</h1>
-      <button @click="hanfleIncrement">Add</button>
+  <div>
+    <h1>{{ state.count }}</h1>
+    <button @click="hanfleIncrement">Add</button>
+
+
+    <div class="flex items-center gap-4">
+    <div 
+    :class="{active: activeTab === index}"
+    @click.prevent="activeTab = index"
+    v-for="({Text},index) in Tabs" :key="index"  class="w-[200px] h-[50px] bg-blue-800 text-white rounded-xl flex items-center justify-center cursor-pointer hover:bg-red-700 transition-all duration-300 ease">{{ Text }}</div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-
-const state = reactive({count:0});
+import { reactive, ref } from "vue";
+const state = reactive({ count: 0 });
+const activeTab = ref<number>(0);
 
 
 const hanfleIncrement = () => {
-    state.count++;
+  state.count++;
+};
+
+
+
+interface TabInfo {
+  Text: string,
 }
+const Tabs = ref<TabInfo[]>([
+  { Text: "HAN"},
+  { Text: "Nana"},
+  { Text: "Yaw"},
+
+]);
+
 
 </script>
 
 <style scoped>
-
+.active{
+  background: red;
+  color: white;
+}
 </style>

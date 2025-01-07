ec5c9d90673c1f838b129a11d3bc7cbeb61340cb
gyamficodes <gyamfijohn08@gmail.com>
2025-01-07 03:23:37 +0000

    Learning

---
+++ src/components/Listing.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="w-4 h-4 p-3 bg-[#ccc] border" @click="greeting = !greeting">
+      Greeting
+    </button>
+    <h1 v-if="greeting">Good moring</h1>
+    <h1 v-else>Good Afternoon</h1>
+
+    <div class="flex flex-col gap-4">
+      <div v-if="myType === 'A'">I like A</div>
+      <div v-else-if="myType === 'B'">I like B</div>
+      <div v-else-if="myType === 'C'">I like C</div>
+      <div v-else>Not A/B/C</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const greeting = ref<boolean>(true);
+
+const myType = ref<string>("A");
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Listing from "@/components/Listing.vue";
 import HelloWorld from "../components/HelloWorld.vue"
 import Lesson from "@/components/Lesson.vue";
 </script>
@@ -6,6 +7,7 @@ import Lesson from "@/components/Lesson.vue";
 <template>
    <HelloWorld/>
    <Lesson/>
+   <Listing/>
 </template>
 
 <style></style>

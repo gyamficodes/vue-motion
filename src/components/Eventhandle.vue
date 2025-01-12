ccef829296cf6cace51183b2c5e1a1cbd4e43cfc
gyamficodes <gyamfijohn08@gmail.com>
2025-01-12 07:31:59 +0000

    leraning form binding etc

--- src/components/Eventhandle.vue
+++ src/components/Eventhandle.vue
@@ -2,25 +2,39 @@
   <div>
     <button @click="count++">Add Data</button>
     <h2>{{ count }}</h2>
-    <button @click="HanddleMessage('Hello World')">SowMessage</button>
-
+    <button @keydown="HanddleMessage('Hello World')">SowMessage</button>
 
     <!-- the click event's propagation will be stopped -->
-<!-- <a @click.stop="doThis"></a> -->
+    <!-- <a @click.stop="doThis"></a> -->
 
-<!-- the submit event will no longer reload the page -->
-<!-- <form @submit.prevent="onSubmit"></form> -->
+    <!-- the submit event will no longer reload the page -->
+    <!-- <form @submit.prevent="onSubmit"></form> -->
 
-<!-- modifiers can be chained -->
-<!-- <a @click.stop.prevent="doThat"></a> -->
+    <!-- modifiers can be chained -->
+    <!-- <a @click.stop.prevent="doThat"></a> -->
 
-<!-- just the modifier -->
-<!-- <form @submit.prevent></form> -->
+    <!-- just the modifier -->
+    <!-- <form @submit.prevent></form> -->
 
-<!-- only trigger handler if event.target is the element itself -->
-<!-- i.e. not from a child element -->
-<!-- <div @click.self="doThat">...</div>
+    <!-- only trigger handler if event.target is the element itself -->
+    <!-- i.e. not from a child element -->
+    <!-- <div @click.self="doThat">...</div>
   -->
+
+    <!-- Forms Binding  -->
+    <br />
+    <input type="text" v-model="Message" placeholder="Edit Me" />
+    <p>{{ Message }}</p>
+
+    <span>Multiline message is:</span>
+    <p style="white-space: pre-line">{{ news }}</p>
+    <textarea v-model="news" placeholder="add multiple lines"></textarea>
+
+    <!-- Dealing with checkbox -->
+<br>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <button class=" px-3 py-3 border" @click="handdleSucscribe">Subscribe</button>
+    <h1 :class="checked ? 'text-2xl text-green-600' : 'text-2xl text-red-600'" >{{ SubscribesMessage }}</h1>
   </div>
 </template>
 
@@ -32,8 +46,22 @@ const HanddleMessage = (message: string) => {
   alert(message);
 };
 
-// event modifires
+const Message = ref<string>("");
 
+const news = ref<string>("");
+
+const checked = ref<boolean>(false);
+
+const SubscribesMessage = ref<string>("");
+
+const handdleSucscribe = () => {
+  const checkbox = document.getElementById("checkbox") as HTMLInputElement;
+  if (checkbox && checkbox.checked) {
+    SubscribesMessage.value = "You have Subscribe Succesfully to the channel";
+  } else {
+    SubscribesMessage.value = "You have not Subscribe Yet , And you can do that by pressing the button below.";
+  }
+};
 </script>
 
 <style scoped></style>

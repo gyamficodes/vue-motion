adb59a89a2110c0bbab990275024d5baedb6d75a
gyamficodes <gyamfijohn08@gmail.com>
2025-01-15 03:07:55 +0000

    update

--- src/components/WatchesLesson.vue
+++ src/components/WatchesLesson.vue
@@ -1,40 +1,50 @@
 <template>
   <div>
-  <div>
-    <h1>Ask yes/no questions ?</h1>
-    <input type="text"  v-model="questions" :disabled="loader">
-  </div>
+    <div>
+      <h1>Ask yes/no questions ?</h1>
+      <input type="text" v-model="questions" :disabled="loader" />
+    </div>
 
-<h1>{{ answers }}</h1>
+    <h1>{{ answers }}</h1>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 
-const questions = ref<string>('');
-const answers = ref<string>('Questions usually contain a question mark. ;-)');
-const loader =  ref<boolean>(false);
+const questions = ref<string>("");
+const answers = ref<string>("Questions usually contain a question mark. ;-)");
+const loader = ref<boolean>(false);
 
 // watches code
 
 watch(questions, async (newQuestion, oldQuestion) => {
-    if (newQuestion.includes('?')) {
-        loader.value = true;
-        answers.value = "thinking...";
-        try {
-            const res = await fetch('https://yesno.wtf/api');
-            const data = await res.json(); // Parse the response
-            answers.value = data.answer; // Adjust based on API response
-        } catch (error) {
-            answers.value = 'Error! Could not reach the API. ' + error;
-        } finally {
-            loader.value = false; // Ensure loader is turned off in all cases
-        }
+  if (newQuestion.includes("?")) {
+    loader.value = true;
+    answers.value = "thinking...";
+    try {
+      const res = await fetch("https://yesno.wtf/api");
+      const data = await res.json(); // Parse the response
+      answers.value = data.answer; // Adjust based on API response
+    } catch (error) {
+      answers.value = "Error! Could not reach the API. " + error;
+    } finally {
+      loader.value = false; // Ensure loader is turned off in all cases
     }
+  }
 });
 
+const X = ref<number>(5);
+const y = ref<number>(5);
+const total = ref<number>();
+watch(
+  () => X.value + y.value,
+  (sum) => {
+   console.log(`${sum}`);
+  }
+);
 </script>
 
 <style scoped></style>
-md:h-[500px] 
+md:h-[500px]
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Todo/>
-    <Eventhandle/>
-    <WatchesLesson/>
+    <Todo />
+    <Eventhandle />
+    <WatchesLesson />
   </div>
 </template>
 
 <script setup>
-import Todo from '@/components/Todo.vue';
-import Eventhandle from '@/components/Eventhandle.vue';
-import WatchesLesson from '@/components/WatchesLesson.vue';
+import Todo from "@/components/Todo.vue";
+import Eventhandle from "@/components/Eventhandle.vue";
+import WatchesLesson from "@/components/WatchesLesson.vue";
 </script>
 
-<style>
-
-</style>
+<style></style>

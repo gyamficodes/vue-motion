56341f0d5837e5da64cb94e98f1c3a847e8d6ec5
gyamficodes <gyamfijohn08@gmail.com>
2025-02-07 08:19:30 +0000

    update

--- src/components/SlotComp.vue
+++ src/components/SlotComp.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <header>
-        <slot name="header"></slot>
+      <slot name="header"></slot>
     </header>
 
     <main>
-        <slot></slot>
+      <slot :data="data"></slot>
     </main>
 
+    <main>
+      <slot name="content"></slot>
+    </main>
     <footer>
-        <slot name="footer"></slot>
+      <slot name="footer"></slot>
     </footer>
 
     <!-- conditional slots -->
 
-<!-- 
+    <!-- 
   <div class="card">
     <div v-if="$slots.header" class="card-header">
       <slot name="header" />
@@ -28,11 +31,23 @@
       <slot name="footer" />
     </div>
   </div> -->
-
-
+    <button @click="injectData?.dispayMessage" class=" px-3 bg-red-500 text-white">DispalyMessage</button>
+    <h1>{{ injectData?.Page }}</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// inject this data from learning components
+import { inject, type Ref, ref } from "vue";
+
+const data = ref<string>("John is going to school");
+
+const injectData = inject<{ Page: Ref<number>; dispayMessage: () => void }>( "display");
+if (injectData) {
+  const { Page, dispayMessage } = injectData;
+} else {
+  console.error("Injection failed: 'counter' not found.");
+}
+</script>
 
 <style scoped></style>
--- src/views/LearningView.vue
+++ src/views/LearningView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <SlotComp>
+    <SlotComp  >
+       <template  v-slot="{data}">
+       {{ data }}
+       </template> 
       <template #header> <h1 class=" text-blue-600">This is footer design</h1> </template>
 
-      <template #default>
+      <template #content>
         <p>This will go into the default slot.</p>
       </template>
-      
+
+
+
+
       <template #footer> <p class=" text-blue-600">This is footer design</p>  </template>
     </SlotComp>
   </div>
@@ -14,6 +20,19 @@
 
 <script setup lang="ts">
 import SlotComp from "@/components/SlotComp.vue";
+import { provide, ref } from "vue";
+
+const dispayMessage = () => {
+    alert("This is a message");
+}
+
+const Page = ref<number>(50);
+
+
+provide("display", {dispayMessage, Page});
+
+
+ 
 </script>
 
 <style scoped></style>

2ed709fcd753a116438f8fe87b9fb2152fb6f847
gyamficodes <gyamfijohn08@gmail.com>
2025-03-13 15:58:04 +0000

    vitest for counter component completed.

---
+++ src/components/__tests__/Counter.spec.ts
@@ -0,0 +1,33 @@
+import { mount} from  "@vue/test-utils"
+import Counter from "@/components/VueTestComp/Counter.vue";
+import { describe, it, expect } from "vitest";
+
+
+describe("Counter.vue" , () => {
+   it('renders the correct initial text', () => {
+       const wrapper = mount(Counter);
+       expect(wrapper.text()).toContain('0');
+   })
+
+   it('increments count on click', async () => {
+    const wrapper = mount(Counter);
+     await  wrapper.find('button').trigger('click');
+     expect(wrapper.text()).toContain('1')
+   })
+
+   it('decrements count on click', async () => {
+    const wrapper = mount(Counter);
+    await wrapper.find('button').trigger('click');
+     expect(wrapper.text()).toContain('-1')
+   })
+
+   it('resets count on click', async () => {
+    const wrapper = mount(Counter);
+    await wrapper.find('button').trigger('click');
+     expect(wrapper.text()).toContain('Reset')
+   })
+})
+
+
+
+

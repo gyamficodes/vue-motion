eb8b2b2db803b683b4634d56d05e236d445ec5cb
gyamficodes <gyamfijohn08@gmail.com>
2025-03-21 10:57:45 +0000

    Testing my Props using Vitest

--- src/components/__tests__/Props.spec.ts
+++ src/components/__tests__/Props.spec.ts
@@ -1,3 +1,18 @@
+import PropsComponent from "@/components/PropsComponent.vue";
 import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
 
+describe("PropsComponent.vue", () => {
+  it("renders Props Correctly", () => {
+    const props = {
+      name: "John",
+      Cobro: "Hello WOrld",
+      greetingMessage: "Good Morning",
+      post: { title: "Gyamficodes", content: "Learning Vitest" },
+    };
+    const wrapper = mount(PropsComponent, { props });
+    expect(wrapper.props()).toMatchObject(props);
+  });
 
+  
+});
